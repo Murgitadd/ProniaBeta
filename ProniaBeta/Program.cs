d069f46fafe86c92ff317cd017ffcca593eec338
Murad Xasaddinov <murad.xasaddinov@gmail.com>
2023-12-05 05:38:22 +0400

    Identity Options and Authentification

--- ProniaBeta/Program.cs
+++ ProniaBeta/Program.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProniaBeta;
 using ProniaBeta.DAL;
+using ProniaBeta.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>((options) => { options.UseSqlServer(builder.Configuration.GetConnectionString("Default")); });
-var app = builder.Build();
+builder.Services.AddIdentity<AppUser, IdentityRole>(options => {
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
 
+    options.User.RequireUniqueEmail = true;
+
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseStaticFiles();
 app.UseRouting();
 
---
+++ ProniaBeta/ViewModels/Account/RegisterVM.cs
@@ -0,0 +1,10 @@
+﻿namespace ProniaBeta.ViewModels.Account
+{
+    public class RegisterVM
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string  Name { get; set; }
+    }
+}
