96975bdc5399e62e5054c84eece434ed167f16e1
Murad Xasaddinov <murad.xasaddinov@gmail.com>
2023-12-07 03:38:55 +0400

    Minor Syntax fix

--- ProniaBeta/obj/Debug/net6.0/ProniaBeta.AssemblyInfo.cs
+++ ProniaBeta/obj/Debug/net6.0/ProniaBeta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProniaBeta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d069f46fafe86c92ff317cd017ffcca593eec338")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProniaBeta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProniaBeta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
